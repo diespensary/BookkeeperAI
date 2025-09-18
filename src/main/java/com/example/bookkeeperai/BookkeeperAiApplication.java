72f0d756533af132b350826c8d8d5cbbf77c996a
diespensary <diespensary@icloud.com>
2025-09-18 17:55:57 +0400

    init commit

---
+++ src/main/java/com/example/bookkeeperai/BookkeeperAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookkeeperai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeeperAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookkeeperAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookkeeperai/BookkeeperAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookkeeperai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkeeperAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
