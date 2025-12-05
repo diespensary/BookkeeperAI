3869fcc5d619fdc367d08149278aa097cd80360c
diespensary <diespensary@icloud.com>
2025-12-05 16:01:17 +0400

    settings

--- src/main/java/com/example/bookkeeperai/BookkeeperAiApplication.java
+++ src/main/java/com/example/bookkeeperai/BookkeeperAiApplication.java
@@ -1,13 +1,25 @@
 package com.example.bookkeeperai;
 
+import com.example.bookkeeperai.telegram.ExpenseBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class BookkeeperAiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookkeeperAiApplication.class, args);
-    }
+        ConfigurableApplicationContext ctx =
+                SpringApplication.run(BookkeeperAiApplication.class, args);
 
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            ExpenseBot bot = ctx.getBean(ExpenseBot.class);
+            botsApi.registerBot(bot);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
